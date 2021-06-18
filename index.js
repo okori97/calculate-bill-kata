fafaba4b0c4709de6c917406b620797984afa409
Okori McCalla <okorishakir@gmail.com>
2021-06-18 21:47:10 +0100

    complete

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
-function calculateBill() {
+function calculateBill(price, vat, tip) {
 
+    const amount = price + vat + tip;
+    var gbp = "£"
+    const errorMessage = "error";
+
+   
+
+    if (isNaN(vat)) {
+        return errorMessage;
+    }  else if (price, vat, !tip){
+        return errorMessage;
+    }
+    else {
+        return gbp.concat(amount.toString());
+    }
+
+    
 };
 
 module.exports = calculateBill;
